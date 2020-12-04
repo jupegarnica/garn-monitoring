d27bbcfee902a0adeffcaa282f4ef2acec207720
Jupegarnica <juan@geekshubs.com>
2020-12-04 13:37:22 +0100

    get smtp conf from config.yaml

--- config.ts
+++ config.ts
@@ -1,10 +1,9 @@
-import { YamlLoader } from 'https://deno.land/x/yaml_loader@v0.1.0/mod.ts';
+import { loadYaml } from 'https://deno.land/x/garn_yaml@v0.1.4/mod.ts';
 import { config } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 
 config({ safe: false, export: true });
 
-const yamlLoader = new YamlLoader();
-const conf: any = await yamlLoader.parseFile('./config.yaml');
+const conf: any = await loadYaml('./config.yaml');
 
 if (!conf.urls?.length) {
   throw new Error("No Urls configured, add them to config.yaml");
@@ -12,3 +11,5 @@ if (!conf.urls?.length) {
 
 export const URLS: string[] = conf.urls
 export const DELAY = 1000 * 60 * 1; // 1 min
+
+export const SMTP = conf.smtp
--- mailer.ts
+++ mailer.ts
@@ -1,5 +1,5 @@
 import { SmtpClient } from 'https://deno.land/x/smtp@v0.6.0/mod.ts';
-import  './config.ts'
+import {SMTP} from './config.ts'
 
 const client = new SmtpClient();
 
@@ -11,16 +11,16 @@ interface Email {
 }
 
 export async function sendEmail({
-  from = Deno.env.get('SMTP_FROM') || '',
-  to = Deno.env.get('SMTP_TO') || '',
+  from = SMTP.from || '',
+  to = SMTP.to || '',
   subject,
   content,
 }: Email): Promise<void> {
   await client.connectTLS({
-    hostname: Deno.env.get('SMTP_HOST') || '',
-    port: Number(Deno.env.get('SMTP_PORT') || ''),
-    username: Deno.env.get('SMTP_USERNAME') || '',
-    password: Deno.env.get('SMTP_PASSWORD') || '',
+    hostname: SMTP.host || '',
+    port: Number(SMTP.port || ''),
+    username: SMTP.username || '',
+    password: SMTP.password || '',
   });
 
   await client.send({
