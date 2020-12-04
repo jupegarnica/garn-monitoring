c4744bd54edfc0c7b0047c91f628fdbbae465f85
Jupegarnica <juan@geekshubs.com>
2020-12-04 10:12:42 +0100

    add config.yaml

--- config.ts
+++ config.ts
@@ -1,11 +1,14 @@
-import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
+import { YamlLoader } from 'https://deno.land/x/yaml_loader@v0.1.0/mod.ts';
+import { config } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 
-config({ safe: false, export:true  })
+config({ safe: false, export: true });
 
-export const URLS: string[] = [
-  'http://jupegarnica.com',
-  'http://fail.jupegarnica.com',
-  'http://valencia.io',
-];
+const yamlLoader = new YamlLoader();
+const conf: any = await yamlLoader.parseFile('./config.yaml');
 
+if (!conf.urls?.length) {
+  throw new Error("No Urls configured, add them to config.yaml");
+}
+
+export const URLS: string[] = conf.urls
 export const DELAY = 1000 * 60 * 1; // 1 min
