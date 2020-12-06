8d564e376711497e5a418a664388be028c4fe51f
Jupegarnica <juan@geekshubs.com>
2020-12-06 09:34:50 +0100

    do not kill process

--- monitoring-once.ts
+++ monitoring-once.ts
@@ -1,9 +1,11 @@
 console.clear();
+import { logger } from './logger.ts';
 import { monitor } from './monitor.ts';
 
 await monitor();
 
 
-setTimeout(() => {
-    Deno.exit(0)
-}, 1000 * 120);
+// setTimeout(() => {
+//     logger.warning('PROCESS KILL BY TIMEOUT')
+//     Deno.exit(0)
+// }, 1000 * 60 * 5); // 5 MIN
