cd8f7a146d6775ece86691efb92f843c8af8520c
Jupegarnica <juan@geekshubs.com>
2020-12-08 15:17:16 +0100

    make it work

--- request.ts
+++ request.ts
@@ -1,62 +1,28 @@
-// import { soxa } from 'https://deno.land/x/soxa@1.4/mod.ts';
-// import { CancelToken } from 'https://deno.land/x/soxa@1.4/src/cancel/CancelToken.ts';
+import { soxa } from 'https://deno.land/x/soxa@1.4/mod.ts';
 // import { REQUEST_TIMEOUT } from './config.ts';
-// // const REQUEST_TIMEOUT = 1000;
+const REQUEST_TIMEOUT = 1000;
 
-// export async function get(url: string): Promise<any> {
-//   let cancel: Function;
-//   const id: number = setTimeout(() => {
-//     cancel('TIMEOUT ' + REQUEST_TIMEOUT);
-//     console.log('cancel');
+export async function get(url: string): Promise<any> {
+  return new Promise((resolve, reject)=>{
+    const id: number = setTimeout(() => {
+      reject('TIMEOUT ' + REQUEST_TIMEOUT);
+    }, REQUEST_TIMEOUT);
 
-//   }, REQUEST_TIMEOUT);
-
-//   const response: any = await soxa
-//     .get(url, {
-//       timeout:REQUEST_TIMEOUT,
-//       cancelToken: new CancelToken(
-//         (c: Function) => (cancel = c),
-//       ),
-//     })
-//     .finally(() => clearTimeout(id));
-//   return response;
-// }
-
-
-// @ts-ignore
-function validateStatus(res) {
-  if (res.status >= 200 && res.status < 300) return res;
-  throw res;
-}
-
-// @ts-ignore
-async function fetchWithTimeout(resource, options) {
-  const { timeout = 8000 } = options;
-
-  const controller = new AbortController();
-  const id = setTimeout(() => {
-    console.log('abort');
-    controller.abort();
-
-  }, timeout);
-
-  const response = await fetch(resource, {
-    ...options,
-    signal: controller.signal
+    const req: any = soxa
+      .get(url, {
+        timeout:REQUEST_TIMEOUT,
+      })
+      .then(resolve)
+      .catch(reject)
+      .finally(() => clearTimeout(id));
+    return req;
   })
-  ;
-  clearTimeout(id);
-
-
-  return validateStatus(response);
 }
 
 
 try {
   console.time()
-  const r = await fetchWithTimeout('http://localhost:8000/timeout', {
-    timeout: 500
-  });
+  const r = await get('http://localhost:8000/timeout');
   console.log({r});
 
 } catch (error) {
