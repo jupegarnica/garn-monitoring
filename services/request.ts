b02c4f7ff0d34f2abc795ee85b2ec6c3346bcc3a
Jupegarnica <juan@geekshubs.com>
2021-04-09 15:35:12 +0200

    clean up

--- services/request.ts
+++ services/request.ts
@@ -9,24 +9,3 @@ export function request(request: Request): unknown {
 
   return ky(url, {timeout:REQUEST_TIMEOUT || false, ...options})
 }
-
-
-// export function request(config: any): Promise<any> {
-//   return new Promise((resolve, reject) => {
-//     const id: number = setTimeout(() => {
-//       reject("TIMEOUT " + REQUEST_TIMEOUT);
-//     }, REQUEST_TIMEOUT);
-
-//     const req: any = soxa
-//       .request(
-//         {
-//           timeout: REQUEST_TIMEOUT,
-//           ...config,
-//         },
-//       )
-//       .then(resolve)
-//       .catch(reject)
-//       .finally(() => clearTimeout(id));
-//     return req;
-//   });
-// }
