1cedefe57d193ddce32960dfc90b3a017375cfac
Jupegarnica <juan@geekshubs.com>
2021-04-10 08:42:48 +0200

    clean up

--- borra.js
+++
@@ -1,8 +0,0 @@
-import ky from 'https://unpkg.com/ky/index.js';
-
-ky( "http://localhost:8000/timeout",{
-    method: "GET",
-    url: "http://localhost:8000/timeout",
-    data: "hello",
-    headers: { "content-type": "text/plain" }
-  }).then(console.log)
